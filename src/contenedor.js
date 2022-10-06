dbce274f7030979b9d1bfa1bb36d79168188affd
gustavojcd <g.charalla.dev@gmail.com>
2022-10-06 16:38:46 -0500

    Servidor con Express

---
+++ src/contenedor.js
@@ -0,0 +1,65 @@
+const fs = require('fs')
+const fsPromises = fs.promises
+
+class Contenedor {
+    constructor(filename, prods = []) {
+        this.filename = filename;
+        this.prods = prods;
+        if (!fs.existsSync(this.filename)) {
+            this.writeProductsTxt(this.filename);
+        }
+    }
+    async writeProductsTxt(filename) {
+        try {
+            await fsPromises.writeFile(filename, JSON.stringify(this.prods, null, '\t'));
+        } catch (error) {
+            console.error(error);
+        }
+    }
+    readProductsTxt(filename) {
+        let rawProds = fs.readFileSync(filename, 'utf8');
+        if (rawProds.length === 0) {
+            return this.prods;
+        } else {
+            let newProds = JSON.parse(rawProds);
+            this.prods = [...newProds];
+        }
+    }
+    save(params) {
+        this.readProductsTxt(this.filename)
+        this.prods.push(params)
+        if (this.prods.length === 1) {
+            this.prods[0].id = 1;
+        } else {
+            this.prods[this.prods.length - 1].id = this.prods[this.prods.length - 2].id + 1
+        }
+        this.writeProductsTxt(this.filename);
+    }
+    getById(id) {
+        this.readProductsTxt(this.filename);
+        const index = this.prods.findIndex((prod) => prod.id === id)
+        return index >= 0 ? this.prods[index] : null;
+    }
+    getAll() {
+        this.readProductsTxt(this.filename);
+        return this.prods;
+    }
+    deleteById(id) {
+        this.readProductsTxt(this.filename);
+        for (let x = 0; x < this.prods.length; x++) {
+            if (this.prods[x].id === id) {
+                this.prods.splice(x, 1);
+                this.writeProductsTxt(this.filename);
+                return "el ID ha sido eliminado; archivo actualizado"
+            }
+        }
+        return "el ID ingresado no existe";
+    }
+    deleteAll() {
+        this.prods = [];
+        this.writeProductsTxt(this.filename);
+        return "todos los productos borrados; archivo actualizado"
+    }
+}
+
+module.exports = { Contenedor }
