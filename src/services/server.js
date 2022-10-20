942d5b61a425f82150f0e6e27f1d512448aee8cd
gustavojcd <g.charalla.dev@gmail.com>
2022-10-20 16:44:55 -0500

    pug

--- src/services/server.js
+++ src/services/server.js
@@ -1,9 +1,17 @@
 const express = require('express')
 const router = require('../routes/main')
 const app = express()
+const path = require('path')
 
 app.use(express.static('public'));
 
-app.use('/api', router)
+//inicia la configuracion de Pug
+const viewsFolderPath = path.resolve(__dirname, '../../views');
+
+app.set('views', viewsFolderPath)
+app.set('view engine', 'pug')
+//termina la configuracion de Pug
+
+app.use('/', router)
 
 module.exports = app;
