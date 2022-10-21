5ad2688c275382963cfb2bfac3b1ad5e5a2459f0
gustavojcd <g.charalla.dev@gmail.com>
2022-10-21 09:23:59 -0500

    ejs

--- src/routes/main.js
+++ src/routes/main.js
@@ -3,5 +3,7 @@ const rutasProductos = require('./products')
 const router = Router()
 
 router.use('/productos', rutasProductos);
-
+router.use('/', (req, res) => {
+    res.render('pages/home')
+})
 module.exports = router
--- src/services/server.js
+++ src/services/server.js
@@ -1,9 +1,17 @@
 const express = require('express')
 const router = require('../routes/main')
+const path = require('path')
 const app = express()
+
 
 app.use(express.static('public'));
 
-app.use('/api', router)
+//inicia configuracion de ejs
+const viewsFolderPath = path.resolve(__dirname,'../../views')
+app.set('view engine', 'ejs');
+app.set('views', viewsFolderPath)
+//termina configuracion de ejs
+
+app.use('/', router)
 
 module.exports = app;
